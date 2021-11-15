88a35981d15176397ec266eac73e041e615e9fae
ntoliverapp <n.toliverapplication@gmail.com>
2021-11-15 11:03:18 -0800

    updated sorting

--- ratings.py
+++ ratings.py
@@ -10,15 +10,24 @@ for score in scores:
     keys = score[0]
     values = int(score[1])
     ratings[f'{keys}']=f'{values}'
+    # print(keys, " is rated at ", values)
 
-print(ratings.items())
-print(type(ratings))
+# print(ratings.items())
+# print(type(ratings))
 
 # And finally, spits out the ratings in alphabetical order by restaurant
 
-print(ratings)
+# print(ratings)
 asc_ratings = {}
 
 for pos in sorted(ratings):
     asc_ratings[pos] = ratings[pos]
-print(asc_ratings)
+
+for pos in asc_ratings:
+    print(pos, " is rated at ", asc_ratings[pos])
+    
+    
+
+
+
+
