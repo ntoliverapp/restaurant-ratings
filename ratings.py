de960397c6ed9b7598d1a9d18800900506142abc
ntoliverapp <n.toliverapplication@gmail.com>
2021-11-15 13:21:11 -0800

    updated store and sort, added input

--- ratings.py
+++ ratings.py
@@ -1,33 +1,42 @@
 """Restaurant rating lister."""
 # Reads the ratings in from the file
 scores = open("scores.txt")
+
 # Stores them in a dictionary
 
 ratings = {}
 for score in scores:
     score = score.rstrip('\n').split(':')
+    ratings[score[0]] = int(score[1])
 
-    keys = score[0]
-    values = int(score[1])
-    ratings[f'{keys}']=f'{values}'
-    # print(keys, " is rated at ", values)
 
-# print(ratings.items())
-# print(type(ratings))
+# # print(ratings.items())
+# # print(type(ratings))
 
-# And finally, spits out the ratings in alphabetical order by restaurant
+## And finally, spits out the ratings in alphabetical order by restaurant
 
-# print(ratings)
-asc_ratings = {}
+print(ratings)
+
 
 for pos in sorted(ratings):
-    asc_ratings[pos] = ratings[pos]
+    print(pos, " is rated at ", ratings[pos])
 
-for pos in asc_ratings:
-    print(pos, " is rated at ", asc_ratings[pos])
-    
-    
+## Prompt the user for a restaurant name
 
+new_rating = input("Add a new restaurant name: ")
+
+## Prompt the user for a restaurant score
+
+new_score = int(input("Add a new restaurant score: "))
+
+## Store the new restaurant/rating in the dictionary
+
+ratings[new_rating] = new_score
+## Print all of the ratings in alphabetical order (including the new one, of course)
+
+for pos in sorted(ratings):
+    print(pos, " is rated at ", ratings[pos])
+    
 
 
 
